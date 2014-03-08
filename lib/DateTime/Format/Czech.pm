ce64fb244133d8578c6054382898eafa919411f6
Neil Bowers <neil@bowers.com>
2014-03-08 19:04:00 +0000

    Added COPYRIGHT AND LICENSE section to pod
    Matches the license specified in the metadata

--- lib/DateTime/Format/Czech.pm
+++ lib/DateTime/Format/Czech.pm
@@ -138,6 +138,13 @@ sub format_datetime
 
 Tomáš Znamenáček, zoul@fleuron.cz
 
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by Tomáš Znamenáček, zoul@fleuron.cz
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
 =cut
 
 'SDG';
